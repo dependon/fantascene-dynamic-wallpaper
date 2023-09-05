8dfac11ebf235e22933c80a72b8e1b521e271fa6
Liu Ming Hang <523633637@qq.com>
2023-09-05 11:45:40 +0800

    update sh

---
+++ start_makeLocal.sh
@@ -0,0 +1,6 @@
+sudo apt install qtcreator libx11-xcb-dev libxcb-ewmh-dev libmpv-dev mpv ffmpeg dpkg-dev fakeroot libmtdev-dev libqt5opengl5-dev qttools5-dev-tools qtbase5-dev qt5-qmake qtwebengine5-dev libxext-dev libxrender-dev libqt5x11extras5-dev libglib2.0-dev qt5-default build-essential g++ libxcb-shape0-dev build-essential
+mkdir build
+cd build
+qmake ..
+make -j4
+sudo make install
