81eca14be7cc3ffdc19221cd872275c06df6c62f
dependon <523633637@qq.com>
2023-09-05 17:26:27 +0800

    update

--- obs_debian.sh
+++ obs_debian.sh
@@ -1,6 +1,9 @@
 git clone --depth=1 https://gitee.com/liuminghang/fantascene-dynamic-wallpaper.git
-tar -czvf fantascene-dynamic-wallpaper_1.5.8.orig.tar.gz ./fantascene-dynamic-wallpaper/
-tar -czvf fantascene-dynamic-wallpaper_1.5.8.debian.tar.gz ./fantascene-dynamic-wallpaper/debian
+tar -czvf fantascene-dynamic-wallpaper_1.5.9.orig.tar.gz ./fantascene-dynamic-wallpaper/
+tar -czvf fantascene-dynamic-wallpaper_1.5.9.debian.tar.gz ./fantascene-dynamic-wallpaper/debian
 cd fantascene-dynamic-wallpaper/
-tar -czvf ../fantascene-dynamic-wallpaper_1.5.8.debian.tar.gz ./debian
+tar -czvf ../fantascene-dynamic-wallpaper_1.5.9.debian.tar.gz ./debian
 cd ..
+md5sum fantascene-dynamic-wallpaper_1.5.9.orig.tar.gz
+md5sum fantascene-dynamic-wallpaper_1.5.9.debian.tar.gz
+ls -l
