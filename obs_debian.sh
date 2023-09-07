8df1eebfd911a6b421c0a5fd70e11e83c5e0ac98
depend <523633637@qq.com>
2023-09-07 13:31:05 +0000

    update obs_debian.sh.
    Signed-off-by: depend <523633637@qq.com>

--- obs_debian.sh
+++ obs_debian.sh
@@ -1,6 +1,5 @@
 git clone --depth=1 https://gitee.com/liuminghang/fantascene-dynamic-wallpaper.git
 tar -czvf fantascene-dynamic-wallpaper_1.5.9.orig.tar.gz ./fantascene-dynamic-wallpaper/
-tar -czvf fantascene-dynamic-wallpaper_1.5.9.debian.tar.gz ./fantascene-dynamic-wallpaper/debian
 cd fantascene-dynamic-wallpaper/
 tar -czvf ../fantascene-dynamic-wallpaper_1.5.9.debian.tar.gz ./debian
 cd ..
